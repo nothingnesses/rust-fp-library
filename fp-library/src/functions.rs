65d2e5fbdcf7138b78fccde26e184192ee6e1cca
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2025-07-26 22:37:39 +0100

    update * add `compose` * add `flip` * bump version

--- fp-library/src/functions.rs
+++ fp-library/src/functions.rs
@@ -6,28 +6,42 @@ pub use super::typeclasses::{
 	pure::pure,
 };
 
-/// Returns its input.
+/// Takes a function `f`, returns a new function that takes a function `g`,
+/// then returns the final composed function `f . g`.
 ///
-/// forall a. a -> a
+/// forall a b c. (b -> c) -> (a -> b) -> a -> c
 ///
-/// Examples
+/// # Examples
 ///
 /// ```rust
-/// use fp_library::{functions::identity};
-/// assert_eq!(identity(()), ());
+/// use fp_library::functions::compose;
+///
+/// let add_one = |x: i32| x + 1;
+/// let times_two = |x: i32| x * 2;
+/// let times_two_add_one = compose(add_one)(times_two);
+///
+/// assert_eq!(times_two_add_one(3), 7); // 3 * 2 + 1 = 7
 /// ```
-pub fn identity<A>(a: A) -> A {
-	a
+pub fn compose<'a, A, B, C, F, G>(f: F) -> impl Fn(G) -> Box<dyn Fn(A) -> C + 'a>
+where
+	F: Fn(B) -> C + Clone + 'a,
+	G: Fn(A) -> B + 'a,
+{
+	move |g| {
+		let f = f.to_owned();
+		Box::new(move |a: A| f(g(a)))
+	}
 }
 
 /// Returns its first argument.
 ///
 /// forall a b. a -> b -> a
 ///
-/// Examples
+/// # Examples
 ///
 /// ```rust
 /// use fp_library::{functions::constant};
+///
 /// assert_eq!(constant(true)(false), true);
 /// ```
 pub fn constant<A, B>(a: A) -> impl Fn(B) -> A
@@ -36,4 +50,45 @@ where
 	B: Clone,
 {
 	move |_b| a.to_owned()
+}
+
+/// Returns a curried version of the input binary function,
+/// but with its arguments flipped.
+///
+/// forall a b c. (a -> b -> c) -> b -> a -> c
+///
+/// # Examples
+///
+/// ```rust
+/// use fp_library::functions::flip;
+///
+/// let subtract = |a| move |b| a - b;
+///
+/// assert_eq!(flip(subtract)(1)(0), -1); // 0 - 1 = -1
+/// ```
+pub fn flip<'a, A, B, C, F, G>(f: F) -> impl Fn(B) -> Box<dyn Fn(A) -> C + 'a>
+where
+	B: Clone + 'a,
+	F: Fn(A) -> G + Clone + 'a,
+	G: Fn(B) -> C,
+{
+	move |b| {
+		let f = f.to_owned();
+		Box::new(move |a| (f(a))(b.to_owned()))
+	}
+}
+
+/// Returns its input.
+///
+/// forall a. a -> a
+///
+/// # Examples
+///
+/// ```rust
+/// use fp_library::functions::identity;
+///
+/// assert_eq!(identity(()), ());
+/// ```
+pub fn identity<A>(a: A) -> A {
+	a
 }
