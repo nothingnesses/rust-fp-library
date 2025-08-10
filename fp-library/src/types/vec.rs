ebceae3e2b9ef40f152e75cf3bdb6ac3f8909d5a
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2025-08-10 22:03:19 +0100

    wip traversable

--- fp-library/src/types/vec.rs
+++ fp-library/src/types/vec.rs
@@ -4,7 +4,7 @@ pub mod concrete_vec;
 
 use crate::{
 	aliases::ClonableFn,
-	functions::pure,
+	functions::{apply, map, pure, traverse},
 	hkt::{Apply, Brand, Brand1, Kind, Kind1},
 	impl_brand,
 	typeclasses::{
@@ -18,15 +18,15 @@ use std::sync::Arc;
 
 impl_brand!(VecBrand, Vec, Kind1, Brand1, (A));
 
-impl VecBrand {
-	pub fn construct<A>(head: A) -> impl Fn(Vec<A>) -> Vec<A>
+impl<'a> VecBrand {
+	pub fn construct<A>(head: A) -> ClonableFn<'a, Apply<Self, (A,)>, Apply<Self, (A,)>>
 	where
-		A: Clone,
+		A: 'a + Clone,
 	{
-		move |tail| [vec![head.to_owned()], tail].concat()
+		Arc::new(move |tail| [vec![head.to_owned()], tail].concat())
 	}
 
-	pub fn deconstruct<A>(slice: &[A]) -> Option<Pair<A, Vec<A>>>
+	pub fn deconstruct<A>(slice: &[A]) -> Option<Pair<A, Apply<Self, (A,)>>>
 	where
 		A: Clone,
 	{
@@ -260,22 +260,76 @@ impl Foldable for VecBrand {
 }
 
 impl Traversable for VecBrand {
-	/// Vec.empty = pure Vec.empty
-	/// Vec.constructor a as = (apply ((map Vec.constructor) (f a))) ((traverse f) as)
+	/// traverse f Vec.empty = pure Vec.empty
+	/// traverse f (Vec.construct head tail) = (apply ((map Vec.construct) (f head))) ((traverse f) tail)
 	fn traverse<'a, F, A, B>(
 		f: ClonableFn<'a, A, Apply<F, (B,)>>
 	) -> ClonableFn<'a, Apply<Self, (A,)>, Apply<F, (Apply<Self, (B,)>,)>>
 	where
 		Self: Kind<(A,)> + Kind<(B,)> + Kind<(Apply<F, (B,)>,)>,
-		F: 'a + Kind<(B,)> + Kind<(Apply<Self, (B,)>,)> + Applicative,
+		F: 'a
+			+ Kind<(B,)>
+			+ Kind<(Apply<Self, (B,)>,)>
+			+ Applicative
+			+ Kind1<A>
+			+ Kind1<ClonableFn<'a, Apply<Self, (A,)>, Apply<Self, (A,)>>>
+			+ Kind1<ClonableFn<'a, Apply<Self, (B,)>, Apply<Self, (B,)>>>
+			+ Kind1<Apply<F, (ClonableFn<'a, Apply<Self, (B,)>, Apply<Self, (B,)>>,)>>,
 		A: 'a + Clone,
 		B: Clone,
-		Apply<F, (B,)>: 'a,
+		Apply<F, (B,)>: 'a + Clone,
+		Apply<Self, (B,)>: Clone,
+		Apply<F, (ClonableFn<'a, Apply<Self, (B,)>, Apply<Self, (B,)>>,)>:
+			Clone + Fn(<VecBrand as Kind<(B,)>>::Output),
+		Apply<F, (Apply<F, (ClonableFn<'a, Apply<Self, (B,)>, Apply<Self, (B,)>>,)>,)>: Clone,
 	{
 		Arc::new(move |ta| {
 			match VecBrand::deconstruct(&(<Self as Brand<Vec<A>, _>>::project(ta))) {
-				Some(_) => todo!(),
 				None => pure::<F, _>(<Self as Brand<_, (B,)>>::inject(vec![])),
+				Some(Pair(head, tail)) => {
+					// cons: a -> (t a -> t a)
+					let cons: ClonableFn<
+						'a,
+						A,
+						ClonableFn<'a, Apply<Self, (A,)>, Apply<Self, (A,)>>,
+					> = Arc::new(VecBrand::construct);
+					// map: (a -> b) -> f a -> f b
+					// cons: a -> (t a -> t a)
+					// map cons = f a -> f (t a -> t a)
+					let map_cons: ClonableFn<
+						'a,
+						Apply<F, (_,)>,
+						Apply<F, (ClonableFn<'a, Apply<Self, (_,)>, Apply<Self, (_,)>>,)>,
+					> = Arc::new(map(cons));
+					// f: a -> f b
+					// head: a
+					// f head: f b
+					let f_head: Apply<F, (B,)> = f(head);
+					// traverse: (a -> f b) -> t a -> f (t b)
+					// f: a -> f b
+					// traverse f: t a -> f (t b)
+					// tail: t a
+					// (traverse f) tail: f (t b)
+					let traverse_f_tail: Apply<F, (Apply<Self, (B,)>,)> = traverse(f)(tail);
+					// map cons: f a -> f (t a -> t a)
+					// f head: f b
+					// (map cons) (f head): f (t b -> t b)
+					let map_cons_f_head: Apply<
+						F,
+						(ClonableFn<'a, Apply<Self, (_,)>, Apply<Self, (_,)>>,),
+					> = map_cons(f_head);
+					// apply: f (a -> b) -> f a -> f b
+					// (map cons) (f head): f (t b -> t b)
+					// apply ((map cons) (f head)): f (t b) -> f (t b)
+					// (traverse f) tail: f (t b)
+					// apply ((map cons) (f head)) ((traverse f) tail): f (t b)
+					apply::<
+						F,
+						Apply<F, (ClonableFn<'a, Apply<Self, (B,)>, Apply<Self, (B,)>>,)>,
+						Apply<Self, (B,)>,
+						Apply<Self, (B,)>,
+					>(map_cons_f_head)(traverse_f_tail)
+				}
 			}
 		})
 	}
